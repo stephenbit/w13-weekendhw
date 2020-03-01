40e404d61de6050860b7622c0d858e3fdc360435
Stephen Ramsay <rabrandom@gmail.com>
2020-03-01 11:41:16 +0000

    done some code

--- library.js
+++ library.js
@@ -7,6 +7,20 @@ class Library {
     return this.books.length;
   }
 
+  addBook(newBook) {
+    this.books.push(newBook);
+  };
+
+  addBooks(newBooks) {
+  newBooks.forEach(book => this.books.push(book));
+  };
+
+  printInventory() {
+  this.books.forEach((book) => {
+    console.log(`${book.title} by ${book.author}`);
+    });
+  };
+
 
 // const Library = function (books = []) {
 //   this.books = books;
