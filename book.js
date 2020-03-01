de8c4bf252d77fb07728b04a5af899d963de2ded
Stephen Ramsay <rabrandom@gmail.com>
2020-03-01 11:06:42 +0000

    files

---
+++ book.js
@@ -0,0 +1,22 @@
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+// }
+
+class Book {
+  constructor(author, title, genre) {
+  this.author = author;
+  this.title = title;
+  this.genre = genre;
+  }
+}
+
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+
+module.exports = Book;
---
+++ library.js
@@ -0,0 +1,34 @@
+class Library {
+  constructor(books = []) {
+    this.books = books;
+  }
+
+  bookCount() {
+    return this.books.length;
+  }
+
+
+// const Library = function (books = []) {
+//   this.books = books;
+// };
+
+// Library.prototype.bookCount = function () {
+//   return this.books.length;
+// };
+
+// Library.prototype.addBook = function (newBook) {
+//   this.books.push(newBook);
+// };
+
+// Library.prototype.addBooks = function (newBooks) {
+//   newBooks.forEach(book => this.books.push(book));
+// };
+
+// Library.prototype.printInventory = function () {
+//   this.books.forEach((book) => {
+//     console.log(`${book.title} by ${book.author}`);
+//   });
+// };
+}
+
+module.exports = Library;
